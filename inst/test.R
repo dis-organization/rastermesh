b0626bb5b46d11a4ac5e068bef3ae8f8934e6592
Michael Sumner <mdsumner@gmail.com>
2015-12-27 11:17:22 +1100

    looking good Gladys

--- inst/test.R
+++ inst/test.R
@@ -1,11 +1,13 @@
-## r <- rastermesh("data/mer_his_1992_01.nc")
+r <- rastermesh("data/mer_his_1992_01.nc")
 
 library(raadtools)
+library(dplyr)
 sst <- sstfiles()
 
 r <- NetCDF(sst$fullname[1])
+r <- NetCDF("data/mer_his_1992_01.nc")
 var0 <- r$var$name
 ## dimensions and name of the this variable
-for (i in seq_along(var0)) 
+for (i in seq_along(var0))
   print(r$var  %>% filter(name == var0[i])  %>% inner_join(r$vardim)     %>% select(dimids)  %>% inner_join(r$dims, c("dimids" = "id")))
 
